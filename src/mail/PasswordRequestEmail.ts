be50c91435587f2c4d5e1b9b5e4a89a24920bfc1
sbkl <sebastien@sbkl.ltd>
2022-01-28 10:39:16 +0800

    Deploy 1.4.0

--- src/mail/PasswordRequestEmail.ts
+++ src/mail/PasswordRequestEmail.ts
@@ -3,7 +3,7 @@ import { Mailer, MailInput } from "./Mailer";
 export class PasswordRequestEmail extends Mailer {
   constructor(options: MailInput & { code: string }) {
     super(options);
-    this.subject = "Login request";
+    this.subject = "Welcome to Tickedin";
     this.lines = [
       "Please use the 6 digits code below to login to your account:",
       options.code,
